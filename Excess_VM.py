485008d264bcb0ea3c16af1d32c14fc23c8b2699
rahidaliyev <rahidaliyev18@gmail.com>
2024-02-06 03:16:22 +0400

    Redundant lines deleted

--- Excess_VM.py
+++ Excess_VM.py
@@ -1,10 +1,9 @@
 class Excess_VM:
-    def __init__(self,Date,e_Grid_kWh,Consumption,Excess_Energy_1MW,Excess_1MW,Discharge,Charging_of_battery,Additional_energy):
+    def __init__(self,Date,e_Grid_kWh,Consumption,Excess_Energy_1MW,Excess_1MW,Discharge,Additional_energy):
         self.Date = Date
         self.e_Grid_kWh = e_Grid_kWh
         self.Consumption = Consumption
         self.Excess_Energy_1MW = Excess_Energy_1MW
         self.Excess_1MW = Excess_1MW
         self.Discharge =Discharge
-        self.Charging_of_battery=Charging_of_battery
         self.Additional_energy = Additional_energy
